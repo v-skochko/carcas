8eee2b6fa0e631e34141106c27620e4dd564e3ea
Vskochko <skochkovs@gmail.com>
2014-06-23 12:37:27 +0400

    installer

--- css/css.php
+++ css/css.php
@@ -1,4 +1,6 @@
 <?php
+$dev = $_GET['dev'];
+header ('Content-type: text/css; charset=UTF-8');
 $styles = array(
     "style.css"
 );
@@ -6,24 +8,27 @@ $mincss = "";
 foreach ($styles as $css) {
     $mincss .= file_get_contents($css);
 }
-$mincss = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $mincss);
-$mincss = str_replace(': ', ':', $mincss);
-$mincss = str_replace(array("\r\n",'    ','     '), '', $mincss);
-$mincss = str_replace(';}','}', $mincss);
-$modified = 0;
-$offset = 60 * 60 * 24 * 7;
-if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $modified) {
-    header("HTTP/1.0 304 Not Modified");
-    header ('Cache-Control:');
+if(isset($dev)) {
+    echo $mincss;
 } else {
-    header ('Cache-Control: max-age=' . $offset);
-    header ('Content-type: text/css; charset=UTF-8');
-    header ('Pragma:');
-    header ("Last-Modified: ".gmdate("D, d M Y H:i:s", $modified )." GMT");
-    if(extension_loaded('zlib')){ob_start('ob_gzhandler');}
+    $mincss = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $mincss);
+    $mincss = str_replace(': ', ':', $mincss);
+    $mincss = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $mincss);
+    $mincss = str_replace(';}','}', $mincss);
+    $modified = 0;
+    $offset = 60 * 60 * 24 * 7;
+    if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $modified) {
+        header("HTTP/1.0 304 Not Modified");
+        header ('Cache-Control:');
+    } else {
+        header ('Cache-Control: max-age=' . $offset);
+        header ('Pragma:');
+        header ("Last-Modified: ".gmdate("D, d M Y H:i:s", $modified )." GMT");
+        if(extension_loaded('zlib')){ob_start('ob_gzhandler');}
         ob_start("compress");
-            echo $mincss;
+        echo $mincss;
         ob_end_flush();
-    if(extension_loaded('zlib')){ob_end_flush();}
+        if(extension_loaded('zlib')){ob_end_flush();}
+    }
 }
 ?>
