a45c722a323520f448ba7c67fcde60e37497fbfb
Vskochko <skochkovs@gmail.com>
2014-06-23 12:35:28 +0400

    dellete old files

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="searchform" action="<?php echo site_url(); ?>/">
-	<input type="text" name="s" placeholder="Search" value="<?php $s; ?>" />
-	<input type="submit" value="Search" />
-</form>
