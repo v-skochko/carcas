2da890f1494c3de78da3a3b6a5d176df104e4682
Skochko <Skochkocvs@gmail.com>
2014-09-11 17:59:34 +0300

    Add Google map shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -1,5 +1,39 @@
 <?php
 
+// Google map shortcode
+// add_shortcode('googlemap', 'customgooglemap');
+function customgooglemap($atts) {
+     extract(shortcode_atts(array(
+     "coordinates" => '41.871836, -87.617486'
+     ), $atts));
+     return '<script src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=en"></script>
+    <script>
+        var map;
+        function initialize() {
+            var myLatlng = new google.maps.LatLng('.$coordinates.');
+            var mapOptions = {
+                zoom: 16,
+                center: myLatlng,
+                zoomControl: false,
+                scaleControl: false,
+                scrollwheel: false,
+            };
+            var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                animation: google.maps.Animation.DROP
+            });
+            google.maps.event.addListener(map, "center_changed", function() {
+                window.setTimeout(function() {
+                  map.panTo(marker.getPosition());
+                }, 15000);
+            });
+        };
+        google.maps.event.addDomListener(window, "load", initialize);
+    </script><div id="map_canvas"></div>';
+}
+
 
 
 //User can enter e-mail for ligin
