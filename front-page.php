4995e12683bc415a3d14843d9141f0dd6adb181b
Skochko <Skochkovs@gmail.com>
2015-01-15 17:07:14 +0200

    Add custom_icons

--- front-page.php
+++ front-page.php
@@ -2,8 +2,10 @@
 global $post; ?>
     <section class="content row cfx">
         <article>
-<input type="text">
-
+			<input type="text">
+			<div class="wpcf7 icf-home">
+				<div class="wpcf7-not-valid-tip"></div>
+			</div>
         </article>
     </section>
 <?php get_footer(); ?>
