1ddcb3392449a6536ab3af18dbed8aabb31ecca5
Skochko <Skochkovs@gmail.com>
2015-01-15 18:15:36 +0200

    Plugin Update

--- logic/plugins/init.php
+++ logic/plugins/init.php
@@ -11,7 +11,14 @@
         else if($textlat) return str_replace($lat, $cyr, $textlat);
             else return null;
     }
-// Cyr to Lat for URL; Plugin URI: http://wordpress.org/plugins/cyr3lat/
+/*
+Plugin Name: Cyr to Lat enhanced
+Plugin URI: http://wordpress.org/plugins/cyr3lat/
+Description: Converts Cyrillic, European and Georgian characters in post, term slugs and media file names to Latin characters. Useful for creating human-readable URLs. Based on the original plugin by Anton Skorobogatov.
+Author: Sol, Sergey Biryukov, Nikolay Karev, Dmitri Gogelia
+Author URI: http://karevn.com/
+Version: 3.5
+*/
 function ctl_sanitize_title($title) {
     global $wpdb;
     $iso9_table = array(
@@ -107,29 +114,49 @@ function ctl_schedule_conversion() {
     add_action('shutdown', 'ctl_convert_existing_slugs');
 }
 register_activation_hook(__FILE__, 'ctl_schedule_conversion');
-//Plugin installer;  Plugin URI: https://github.com/thomasgriffin/TGM-Plugin-Activation
-require_once('auto-install.php');
+
+require_once('TGM-Plugin.php');
+/**
+ * This file represents an example of the code that themes would use to register
+ * the required plugins.
+ *
+ * It is expected that theme authors would copy and paste this code into their
+ * functions.php file, and amend to suit.
+ *
+ * @package    TGM-Plugin-Activation
+ * @subpackage Example
+ * @version    2.4.0
+ * @author     Thomas Griffin <thomasgriffinmedia.com>
+ * @author     Gary Jones <gamajo.com>
+ * @copyright  Copyright (c) 2014, Thomas Griffin
+ * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+ * @link       https://github.com/thomasgriffin/TGM-Plugin-Activation
+ */
+/**
+ * Include the TGM_Plugin_Activation class.
+ */
+// require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+/**
+ * Register the required plugins for this theme.
+ *
+ * In this example, we register two plugins - one included with the TGMPA library
+ * and one from the .org repo.
+ *
+ * The variable passed to tgmpa_register_plugins() should be an array of plugin
+ * arrays.
+ *
+ * This function is hooked into tgmpa_init, which is fired within the
+ * TGM_Plugin_Activation class constructor.
+ */
 function my_theme_register_required_plugins() {
+    /**
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
     $plugins = array(
-//        array(
-//            'name'               => 'TGM Example Plugin', // The plugin name.
-//            'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-//            'source'             => get_stylesheet_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
-//            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-//            'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-//            'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-//            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-//            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-//        ),
-//        array(
-//            'name'               => 'TGM New Media Plugin', // The plugin name.
-//            'slug'               => 'tgm-new-media-plugin', // The plugin slug (typically the folder name).
-//            'source'             => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', // The plugin source.
-//            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-//            'external_url'       => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
-//        ),
-        array(
+        // This is an example of how to include a plugin from the WordPress Plugin Repository.
+      array(
             'name'      => 'Contact Form 7',
             'slug'      => 'contact-form-7',
             'required'  => false,
@@ -137,11 +164,6 @@ function my_theme_register_required_plugins() {
             array(
             'name'      => 'WP Migrate DB',
             'slug'      => 'wp-migrate-db',
-            'required'  => false,
-        ),
-            array(
-            'name'      => 'BackWPup Free',
-            'slug'      => 'backwpup',
             'required'  => false,
         ),
         array(
@@ -173,10 +195,16 @@ function my_theme_register_required_plugins() {
             'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
             'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
             'external_url'       => 'http://www.advancedcustomfields.com/pro/', // If set, overrides default API URL and points to an external URL.
-        ),
+        )
     );
+    /**
+     * Array of configuration settings. Amend each line as needed.
+     * If you want the default strings to be available under your own theme domain,
+     * leave the strings uncommented.
+     * Some of the strings are added into a sprintf, so see the comments at the
+     * end of each line for what each argument will be.
+     */
     $config = array(
-        'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
         'default_path' => '',                      // Default absolute path to pre-packaged plugins.
         'menu'         => 'tgmpa-install-plugins', // Menu slug.
         'has_notices'  => true,                    // Show admin notices or not.
@@ -189,16 +217,16 @@ function my_theme_register_required_plugins() {
             'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
             'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
             'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
-            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'tgmpa' ), // %1$s = plugin name(s).
-            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'tgmpa' ), // %1$s = plugin name(s).
-            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'tgmpa' ),
-            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'tgmpa' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
             'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
             'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
             'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
