5af51d9184b8f13cab76dbae7d1253d2dbc32726
Skochko <Skochkovs@gmail.com>
2015-01-23 10:03:09 +0200

    UPD

--- front-page.php
+++ front-page.php
@@ -6,6 +6,11 @@ global $post; ?>
 			<div class="wpcf7 icf-home">
 				<div class="wpcf7-not-valid-tip"></div>
 			</div>
+
+
+
+
+
         </article>
     </section>
 <?php get_footer(); ?>
--- logic/init.js
+++ logic/init.js
@@ -1,29 +1,15 @@
-/*jslint browser: true, white: true*/
-/*global $, jQuery, FastClick*/
 $ = jQuery;
-
-var hash = window.location.hash;
 var ww = document.body.clientWidth;
-
 var mob = false;
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Windows Phone|ZuneWP7|Nokia|Opera Mini/i.test(navigator.userAgent)) { var mob = true; }
-var supportsTouch = false;
-if (window.hasOwnProperty('ontouchstart') || window.navigator.msPointerEnabled) { supportsTouch = true; }
-function footer(e){
+$(document).ready(function() {
     "use strict";
-    $('.footix').height($('footer').outerHeight());
-}
-$(document).ready(function () {
-    "use strict";
-    $(this).on('click', '.wpcf7-not-valid-tip', function(){
+    $(this).on('click', '.wpcf7-not-valid-tip', function() {
         $(this).prev().trigger('focus');
-        $(this).fadeOut(500,function(){
+        $(this).fadeOut(500, function() {
             $(this).remove();
         });
     });
-});
-
-$(window).load(function(){
+}); /* end ready*/
+$(window).load(function() {
     "use strict";
 });
-
