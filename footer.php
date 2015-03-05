71e35403a641251243b5d4235dab2ab7cad9e220
skochko <skochkovs@gmail.com>
2015-03-05 11:49:49 +0200

    Update icon font

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="row cfx">
     	<div class="social_link">
-    		<a href="#" class="icf-facebook4"></a>
+    		<a href="#" class="icf-facebook"></a>
     		<a href="#" class="icf-twitter"></a>
     		<a href="#" class="icf-google-plus"></a>
     	</div>
--- functions.php
+++ functions.php
@@ -3,7 +3,11 @@
     require_once('logic/plugins/init.php');
 ## Shortcodes functions
     require_once('shortcodes.php');
-add_image_size( 'blog', '204', '204', true );
+## Uncomit for add custom post type
+    // require_once('custom-cpt.php');
+## Register custom image size
+    add_image_size( 'blog', '204', '204', true );
+
 ## SPEED & SECURITY BOOST
     ### Clean up wp_head()
     remove_action('wp_head', 'feed_links_extra', 3);
@@ -16,8 +20,6 @@ add_image_size( 'blog', '204', '204', true );
     remove_action('wp_head', 'adjacent_posts_rel_link_wp_head' );
     remove_action('wp_head', 'wp_generator'); //Remove WP Generator Meta Tag
     remove_action('wp_head', 'rel_canonical');
-
-
     ###Remove wp version param from any enqueued scripts
     function vc_remove_wp_ver_css_js( $src ) {
         if ( strpos( $src, 'ver=' ) )
@@ -26,68 +28,38 @@ add_image_size( 'blog', '204', '204', true );
     }
     add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
     add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
-
 ##THEME SETINGS
-    ### Uncomit for add custom post type
-    // require_once('custom-cpt.php');
 
     ### Registered jQuery,  css and js file
-    // function tt_add_scripts() {
-    // if (!is_admin()) {
-    //     wp_deregister_script( 'jquery' );
-    //     wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
-    //     wp_enqueue_script( 'jquery' );
-    // }
-    // wp_enqueue_script('lib_min', theme().'/logic/lib.js', array('jquery'), '', true );
-    // wp_enqueue_script('js_init', theme().'/logic/init.js', array('jquery'), '', true );
-    // //style.css
-    // wp_register_style('base_style', theme().'/style/style.css');
-    // wp_enqueue_style('base_style');
-    // //custom.css
-    // wp_register_style('custom_style', theme().'/style/lib/custom_icon.css');
-    // wp_enqueue_style('custom_style');
-    // }
-    // add_action('wp_enqueue_scripts', 'tt_add_scripts');
-
-
-
-//Custom JS/Styles
-function style_js() {
-    wp_deregister_style( 'contact-form-7' );
-    if (!is_admin()) {
-        wp_deregister_script( 'jquery' );
-        wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
-        wp_enqueue_script( 'jquery' );
+    function style_js() {
+        wp_deregister_style( 'contact-form-7' );
+        if (!is_admin()) {
+            wp_deregister_script( 'jquery' );
+            wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
+            wp_enqueue_script( 'jquery' );
+        }
+        // wp_enqueue_script('googlemaps', '//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false', array(), '', FALSE);
+        wp_enqueue_script('libs', get_template_directory_uri().'/logic/lib.js', array('jquery'), '1.0', true);
+        wp_enqueue_script('init', get_template_directory_uri().'/logic/init.js', array('jquery'), '1.0', true);
+        wp_enqueue_style('reset', get_template_directory_uri() . '/style/lib/_reset.scss');
+        wp_enqueue_style('lib', get_template_directory_uri() . '/style/lib/_lib.scss');
+        wp_enqueue_style('style', get_template_directory_uri() . '/style/style.scss');
     }
-    // wp_enqueue_script('googlemaps', '//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false', array(), '', FALSE);
-    wp_enqueue_script('libs', get_template_directory_uri().'/logic/lib.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('init', get_template_directory_uri().'/logic/init.js', array('jquery'), '1.0', true);
-    wp_enqueue_style('reset', get_template_directory_uri() . '/style/lib/_reset.scss');
-    wp_enqueue_style('lib', get_template_directory_uri() . '/style/lib/_lib.scss');
-    wp_enqueue_style('style', get_template_directory_uri() . '/style/style.scss');
-}
-add_action('wp_enqueue_scripts', 'style_js');
-
-
-
-
+    add_action('wp_enqueue_scripts', 'style_js');
     ### Option Update
         ### Set permalink settings
         function set_permalink(){
               global $wp_rewrite;
               $wp_rewrite->set_permalink_structure('%postname%'); }
         add_action('after_switch_theme', 'set_permalink');
-
         ### Uploads organize into month- and year-based folders
         function uploads_folder_update(){
               update_option('uploads_use_yearmonth_folders', 1); }
         add_action('after_switch_theme', 'uploads_folder_update');
-
         ### Image default link type
         function image_link_t(){
                update_option('image_default_link_type','none'); }
         add_action('after_switch_theme', 'image_link_t');
-
         ### For coments: remove Email & Url fields
          function require_name(){
               update_option('require_name_email', 0); }
@@ -97,7 +69,6 @@ add_action('wp_enqueue_scripts', 'style_js');
             unset($fields['url']);
             return $fields; }
         add_filter('comment_form_default_fields', 'rem_form_fields');
-
     ### Add wiget area
     $bar = array(
         'name'          => 'Sidebar',
@@ -109,13 +80,11 @@ add_action('wp_enqueue_scripts', 'style_js');
         'after_title'   => '</div>'
     );
     register_sidebar($bar);
-
     ### Remove ID in menu list
     add_filter('nav_menu_item_id', 'clear_nav_menu_item_id', 10, 3);
     function clear_nav_menu_item_id($id, $item, $args) {
         return "";
     }
-
     ### Classes for First & Last menu items
     function wpb_first_and_last_menu_class($items) {
     $items[1]->classes[] = 'first';
@@ -123,7 +92,6 @@ add_action('wp_enqueue_scripts', 'style_js');
     return $items;
     }
     add_filter('wp_nav_menu_objects', 'wpb_first_and_last_menu_class');
-
     ### Add ie conditional html5 shim to header (only for old IE)
     function wp_IEhtml5_js () {
         global $is_IE;
@@ -131,10 +99,8 @@ add_action('wp_enqueue_scripts', 'style_js');
             echo '<!--[if lt IE 9]><script src="##html5shim.googlecode.com/svn/trunk/html5.js"></script><script src="##css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->';
     }
     add_action('wp_head', 'wp_IEhtml5_js');
-
     ### Add class to BODY
-
-function new_body_classes( $classes ){
+    function new_body_classes( $classes ){
     if( is_page() ){
         global $post;
         $temp = get_page_template();
@@ -225,21 +191,17 @@ add_filter( 'body_class', 'new_body_classes' );
              wp_title('-', true, 'right');
          }
          bloginfo('name');}
-
     ###Register menus
     register_nav_menus(array(
         'head_menu' => 'Main navigation',
         'foot_menu' => 'Footer navigation'
     ));
-
     ###Thumbnails theme support
     add_theme_support( 'post-thumbnails' );
-
     ##custom theme url
     function theme(){
     return ($_SERVER['REMOTE_ADDR']=='127.0.0.1'?site_url():'') . str_replace(site_url(), '', get_stylesheet_directory_uri());
     }
-
     ###Disables Kses
     foreach (array('term_description', 'link_description', 'link_notes', 'user_description') as $filter) {
         remove_filter($filter, 'wp_kses_data');
@@ -247,16 +209,13 @@ add_filter( 'body_class', 'new_body_classes' );
       foreach (array('pre_term_description', 'pre_link_description', 'pre_link_notes', 'pre_user_description') as $filter) {
         remove_filter($filter, 'wp_filter_kses');
     }
-
     ### Deregister Contact Form 7 default styles
     add_action( 'wp_print_styles', 'voodoo_deregister_styles', 100 );
     function voodoo_deregister_styles() {
         wp_deregister_style( 'contact-form-7' );
     }
-
     ### Activate ACF option page
    if( function_exists('acf_add_options_page') ) {
-
     acf_add_options_page(array(
         'page_title'    => 'Theme General Settings',
         'menu_title'    => 'Theme Settings',
@@ -264,21 +223,17 @@ add_filter( 'body_class', 'new_body_classes' );
         'capability'    => 'edit_posts',
         'redirect'      => false
     ));
-
     acf_add_options_sub_page(array(
         'page_title'    => 'Theme Header Settings',
         'menu_title'    => 'Header',
         'parent_slug'   => 'theme-general-settings',
     ));
-
     acf_add_options_sub_page(array(
         'page_title'    => 'Theme Footer Settings',
         'menu_title'    => 'Footer',
         'parent_slug'   => 'theme-general-settings',
     ));
-
 }
-
 ## WORDPRESS ADMIN PANEL SETINGS
     ### Remove dashboard wigets
     function rem_dash_widgets() {
@@ -287,26 +242,21 @@ add_filter( 'body_class', 'new_body_classes' );
         remove_meta_box('dashboard_primary', 'dashboard', 'normal');
         remove_meta_box('dashboard_secondary', 'dashboard', 'normal'); }
     add_action('admin_init', 'rem_dash_widgets');
-
     ### Add custom logo to login page
     add_action( 'login_head', 'namespace_login_style' );
     function namespace_login_style() {
     echo '<style>.login h1 a { background-image: url( ' . get_template_directory_uri() . '/img/login-logo.png ) !important;height: 135px!important; }</style>'; }
-
     ### Changing the logo link from wordpress.org to your site
     function tt_login_url() {  return home_url(); }
-
     ### Changing the alt text on the logo to show your site name
     function tt_login_title() { return get_option( 'blogname' ); }
     add_filter( 'login_headerurl', 'tt_login_url' );
     add_filter( 'login_headertitle', 'tt_login_title' );
-
     ### Add custom info to admin footer area
     function remove_footer_admin () {
         echo 'Powered by <a href="http://www.wordpress.org" target="_blank">WordPress </a>  | Theme Developer <a href="https://www.facebook.com/skochko" target="_blank">@skochko</a>';
     }
     add_filter('admin_footer_text', 'remove_footer_admin');
-
     ###Change admin post/page color by status – draft, pending, future, private
     add_action('admin_footer','posts_status_color');
     function posts_status_color(){
@@ -314,13 +264,11 @@ add_filter( 'body_class', 'new_body_classes' );
     <style>
     .status-draft{background: #E6E6E6 !important;}
     .status-pending{background: #E2F0FF !important;}
-    .status-publish{/* no background keep wp alternating colors */}
     .status-future{background: #C6EBF5 !important;}
     .status-private{background:#F2D46F;}
     </style>
     <?php
     }
-
     ### FOR ADMIN ONLY:
         #### Color scheme "Midnight" set as default
         add_filter( 'get_user_option_admin_color', function( $color_scheme ) {
@@ -330,13 +278,13 @@ add_filter( 'body_class', 'new_body_classes' );
             }
              return $color_scheme;
         }, 5 );
-
+        #### Hide the Admin Bar
+        add_filter('show_admin_bar', '__return_false');
         #### Add link to all settings menu
         function all_settings_link() {
          add_options_page(__('All Settings'), __('All Settings'), 'administrator', 'options.php');
         }
         add_action('admin_menu', 'all_settings_link');
-
     ### FOR USERS EXCEPT SYSADMIN:
         #### Remove the wordpress update notification for all users except sysadmin
         global $user_login;
@@ -344,9 +292,6 @@ add_filter( 'body_class', 'new_body_classes' );
         if (!current_user_can('update_plugins')) { ## checks to see if current user can update plugins
          add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
          add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) ); }
-
-
-
     function adm_separator() {
         echo '<style type="text/css">#adminmenu li.wp-menu-separator {margin: 0; height: 2px; background: #26292C;}</style>';
     }
