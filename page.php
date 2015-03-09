f1e398e6dd708c8d7ec3a509d2774301479ca3e3
Skochko <skochkovs@gmail.com>
2015-03-09 17:58:16 +0200

    Update page.php

--- page.php
+++ page.php
@@ -3,6 +3,7 @@ global $post; ?>
     <section class="content row cfx">
         <article>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                the_title( '<h1>', '</h1>' );
                 the_content();
             endwhile; endif; ?>
         </article>
