0641982adefdbea8551a92d709956f30ab6ce385
Skochko <skochkovs@gmail.com>
2015-03-26 10:26:59 +0200

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ ob_start('ob_html_compress');
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title><?php seo_title(); ?></title>
+<title><?php wp_title(); ?></title>
 <meta name="MobileOptimized" content="width" />
 <meta name="HandheldFriendly" content="True"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" />
