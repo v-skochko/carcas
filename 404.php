fd395c99ddf603cac8621fd44612c903fa76f60c
skochko <skochkovs@gmail.com>
2015-05-14 13:01:43 +0300

    upd

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <div id="content" class="row cfx ">
 	<main  role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
         <article>
-        <span class="icf-404"></span>
+        <span class="icf-new-releases"></span>
 			<h1><span>404</span> Article Not Found</h1>
 			<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
 			<p><?php get_search_form(); ?></p>
