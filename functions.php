a9c8cf49c5bb9d266892a2b1597c05e3a0d87c6a
Skochko <skochkovs@gmail.com>
2015-08-25 15:42:02 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,3 +21,22 @@ register_nav_menus(array(
     'second_menu' => 'Second navigation',
     'foot_menu' => 'Footer navigation'
 ));
+
+//excerpt custom  echo gebid($post->ID, '140' );
+function gebid($post_id, $num, $readmore = ''){
+    $the_post = get_post($post_id); //Gets post ID
+    $the_excerpt = strip_tags(strip_shortcodes($the_post->post_content)); //Strips tags and images
+    $more = trim(strip_tags(substr($the_post->post_content, 0, strpos($the_post->post_content, '<!--more'))));//check if post has more tag
+    $words = explode(' ', $the_excerpt, $num + 1);
+    if(count($words) > $num) :
+    array_pop($words);
+    array_push($words, '...');
+    $the_excerpt = implode(' ', $words);
+    endif;
+    if($more != '') {
+        $content = $more;
+    } else {
+        $content = '<p>' . $the_excerpt . '</p>';
+    }
+    return $content . ($readmore != ''?'<p class="readmore"><a href="' . get_permalink($post_id) . '">' . $readmore . '</a></p>':'');
+}
