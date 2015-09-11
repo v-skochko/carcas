c72bd8deb60f76b32859606132e67ef4bc20987f
Skochko <skochkovs@gmail.com>
2015-09-11 10:20:18 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,22 @@
 $ = jQuery;
 $(document).ready(function () {
     "use strict";
+    
+       $('.toogle_nav').click(function() {
+        $(this).next().toggleClass('open_menu');
+        return false;
+    });
+    $(".main_nav_container .menu-item-has-children").append('<i class="icf-down"></i>')
+    $(".menu-item-has-children i").click(function(event) {
+        $(this).prev('.sub-menu').slideToggle("fast");
+    });
+
+    $('[data-background]').each(function() {
+        if ($(this).attr('data-background').length) {
+            $(this).backstretch($(this).attr('data-background'));
+        }
+    });
+    
     //BXslider
     $('.slider ul').bxSlider({
         nextText: "",
