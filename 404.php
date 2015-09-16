ba644cd681f4800c8b27b3dad4f8d8026be6445c
Skochko <skochkovs@gmail.com>
2015-09-16 14:47:14 +0300

    upd

--- 404.php
+++ 404.php
@@ -2,9 +2,32 @@
 <div id="content" class="row cfx ">
     <main role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
         <article>
-            <span class="icf-exclamation"></span>
+<div class='loader'>
+  <div class='loader_overlay'></div>
+  <div class='loader_cogs'>
+    <div class='loader_cogs__top'>
+      <div class='top_part'></div>
+      <div class='top_part'></div>
+      <div class='top_part'></div>
+      <div class='top_hole'></div>
+    </div>
+    <div class='loader_cogs__left'>
+      <div class='left_part'></div>
+      <div class='left_part'></div>
+      <div class='left_part'></div>
+      <div class='left_hole'></div>
+    </div>
+    <div class='loader_cogs__bottom'>
+      <div class='bottom_part'></div>
+      <div class='bottom_part'></div>
+      <div class='bottom_part'></div>
+      <div class='bottom_hole'><!-- lol --></div>
+    </div>
+  </div>
 
-            <h1><span>404</span> Article Not Found</h1>
+</div>
+
+            <h1><i>404</i> Article Not Found</h1>
 
             <p>The article you were looking for was not found, but maybe try looking again!</p>
 
@@ -16,4 +39,8 @@
         </article>
     </main>
 </div>
+
+<style>
+
+</style>
 <?php get_footer(); ?>
