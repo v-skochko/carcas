5b50e4373669acdf41790ccc7a20815facbbf80f
Skochko <skochkovs@gmail.com>
2015-09-16 16:04:54 +0300

    404

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <div id="content" class="row cfx ">
     <main role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
         <article>
-<div class='loader'>
+<!-- <div class='loader'>
   <div class='loader_overlay'></div>
   <div class='loader_cogs'>
     <div class='loader_cogs__top'>
@@ -21,16 +21,19 @@
       <div class='bottom_part'></div>
       <div class='bottom_part'></div>
       <div class='bottom_part'></div>
-      <div class='bottom_hole'><!-- lol --></div>
+      <div class='bottom_hole'></div>
     </div>
   </div>
-
+</div> -->
+<div class="inside_web">
+<div class="l_overlay">
 </div>
-
-            <h1><i>404</i> Article Not Found</h1>
-
+<span class="icf-cog cog_top"></span>
+<span class="icf-cog cog_left"></span>
+<span class="icf-cog cog_right"></span>
+</div>
+            <h1><i>404</i> Page Not Found</h1>
             <p>The article you were looking for was not found, but maybe try looking again!</p>
-
             <form role="search" method="get" class="search404" action="<?php echo home_url('/') ?>">
                 <span class="icf-search"><input placeholder="Search" type="text"
                                                 value="<?php echo get_search_query() ?>" name="s" id="s"/></span>
@@ -39,8 +42,6 @@
         </article>
     </main>
 </div>
-
 <style>
-
 </style>
 <?php get_footer(); ?>
