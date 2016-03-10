45aab103d6d378a7cd14a435a36b3d0d010545d6
Skochko <skochkovs@gmail.com>
2016-03-10 09:43:56 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,6 +12,15 @@ require_once 'include/core.php';
 /* REGISTER CUSTOM IMAGE SIZE
    ========================================================================== */
 // add_image_size( 'custom', '300', '300', true );
+
+//Default Thumbnail Sizes
+// if(get_option('thumbnail_size_w')!=160)update_option('thumbnail_size_w',160);
+// if(get_option('thumbnail_size_h')!=80)update_option('thumbnail_size_h',80);
+// if(get_option('medium_size_w')!=640)update_option('medium_size_w',640);
+// if(get_option('medium_size_h')!=320)update_option('medium_size_h',320);
+// update_option('large_size_w',1920);
+// update_option('large_size_h',640);
+
 /* REGISTER MENUS
    ========================================================================== */
 register_nav_menus(array(
