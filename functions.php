23433ff93449cf76ece8492f3d39a9dd44dc3669
Skochko <skochkovs@gmail.com>
2016-03-10 09:44:56 +0200

    CUSTOM IMAGE SIZE

--- functions.php
+++ functions.php
@@ -9,17 +9,18 @@ require_once 'include/plugins/init.php';
 require_once 'include/core.php';
 // Uncomit for add custom post type
 // require_once('include/custom-cpt.php');
-/* REGISTER CUSTOM IMAGE SIZE
+/*CUSTOM IMAGE SIZE
    ========================================================================== */
 // add_image_size( 'custom', '300', '300', true );
 
 //Default Thumbnail Sizes
-// if(get_option('thumbnail_size_w')!=160)update_option('thumbnail_size_w',160);
-// if(get_option('thumbnail_size_h')!=80)update_option('thumbnail_size_h',80);
-// if(get_option('medium_size_w')!=640)update_option('medium_size_w',640);
-// if(get_option('medium_size_h')!=320)update_option('medium_size_h',320);
+// update_option('thumbnail_size_w',160);
+// update_option('thumbnail_size_h',80);
+// update_option('medium_size_w',640);
+// update_option('medium_size_h',320);
 // update_option('large_size_w',1920);
 // update_option('large_size_h',640);
+
 
 /* REGISTER MENUS
    ========================================================================== */
