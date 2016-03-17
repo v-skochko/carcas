af400dd3e5845b5bafab7f7378d266d57ee74ab1
Skochko <skochkovs@gmail.com>
2016-03-17 15:59:02 +0200

    upd social link field

--- footer.php
+++ footer.php
@@ -4,13 +4,14 @@
         <span class="copyright"><?php echo date('Y'); ?> <?php bloginfo('name'); ?></span>
 
         <div class="social_link">
-            <a href="<?php if (get_field('facebook')) {the_field('field_name'); } else {echo "https://www.facebook.com/"; } ?>" class="i-facebook" title="Official Facebook page" target="_blank"></a>
-            <a href="<?php if (get_field('instagram')) {the_field('field_name'); } else {echo "https://www.instagram.com/"; } ?>" class="i-instagram"  title="Official Instagram accounts" target="_blank"></a>
-            <a href="<?php if (get_field('twitter')) {the_field('field_name'); } else {echo "https://twitter.com/"; } ?>" class="i-twitter" title="Official Twitter accounts" target="_blank"></a>
-            <a href="<?php if (get_field('google')) {the_field('field_name'); } else {echo "https://plus.google.com/"; } ?>" class="i-g"  title="Official Instagram accounts" target="_blank"></a>
-            <a href="<?php if (get_field('youtube')) {the_field('field_name'); } else {echo "https://www.youtube.com/"; } ?>"  class="i-youtube-play" title="Official Youtube channel"  target="_blank"></a>
-            <a href="<?php if (get_field('pinterest')) {the_field('field_name'); } else {echo "https://pinterest.com/"; } ?>"   class="i-pinterest" title="Official Pinterest accounts"target="_blank"></a>
-            <a href="<?php if (get_field('pinterest')) {the_field('field_name'); } else {echo "https://www.linkedin.com/"; } ?>"   class="i-linkedin" title="Official Linkedin profile" target="_blank"></a>
+            <a href="<?php if (get_field('twitter', 'options')) {the_field('twitter', 'options'); } else {echo "https://twitter.com/"; } ?>" class="i-twitter" title="Official Twitter accounts" target="_blank"></a>
+            <a href="<?php if (get_field('instagram', 'options')) {the_field('instagram', 'options'); } else {echo "https://www.instagram.com/"; } ?>" class="i-instagram"  title="Official Instagram accounts" target="_blank"></a>
+            <a href="<?php if (get_field('facebook', 'options')) {the_field('facebook', 'options'); } else {echo "https://www.facebook.com/"; } ?>" class="i-facebook" title="Official Facebook page" target="_blank"></a>
+            <a href="<?php if (get_field('linkedin', 'options')) {the_field('linkedin', 'options'); } else {echo "https://www.linkedin.com/"; } ?>"   class="i-linkedin" title="Official Linkedin profile" target="_blank"></a>
+            <a href="<?php if (get_field('google', 'options')) {the_field('google', 'options'); } else {echo "https://plus.google.com/"; } ?>" class="i-g"  title="Official Instagram accounts" target="_blank"></a>
+            <a href="<?php if (get_field('pinterest', 'options')) {the_field('pinterest', 'options'); } else {echo "https://pinterest.com/"; } ?>"   class="i-pinterest" title="Official Pinterest accounts"target="_blank"></a>
+            <a href="<?php if (get_field('linkedin', 'options')) {the_field('linkedin', 'options'); } else {echo "https://www.linkedin.com/"; } ?>"   class="i-linkedin" title="Official Linkedin profile" target="_blank"></a>
+            <a href="<?php if (get_field('youtube', 'options')) {the_field('options', 'options'); } else {echo "https://www.youtube.com/"; } ?>"  class="i-youtube-play" title="Official Youtube channel"  target="_blank"></a>
         </div>
     </div>
 </footer>
