d9f0f170bd121a0cca403fe5325bcf487c85f63e
Skochko <Skochkovs@gmail.com>
2016-03-23 18:09:40 +0200

    upd

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php get_header();
 global $post; ?>
     <div id="content" class="row cfx">
-        <main role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+        <main >
             <article>
-                <h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+                <h1 class="page-title" ><?php the_title(); ?></h1>
                 <div class="wysiwyg">
                     <?php if (have_posts()): while (have_posts()): the_post();
                         the_content();
