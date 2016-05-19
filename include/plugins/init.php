b6bbaa077ed8d6cc679a53c896047ee5993b021d
Skochko <Skochkovs@gmail.com>
2016-05-19 11:58:17 +0300

    upadate TGM plugin activator to  2.6.0

--- include/plugins/init.php
+++ include/plugins/init.php
@@ -126,176 +126,106 @@ function ctl_schedule_conversion()
 }
 
 register_activation_hook(__FILE__, 'ctl_schedule_conversion');
-
-
 /**
  * This file represents an example of the code that themes would use to register
  * the required plugins.
- *
- * It is expected that theme authors would copy and paste this code into their
- * functions.php file, and amend to suit.
- *
- * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
- *
- * @package    TGM-Plugin-Activation
- * @subpackage Example
- * @version    2.5.2
- * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
- * @copyright  Copyright (c) 2011, Thomas Griffin
- * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
- * @link       https://github.com/TGMPA/TGM-Plugin-Activation
- */
-
-/**
- * Include the TGM_Plugin_Activation class.
- *
- * Depending on your implementation, you may want to change the include call:
- *
- * Parent Theme:
- * require_once get_template_directory() . '/path/to/class-tgm-plugin-activation.php';
- *
- * Child Theme:
- * require_once get_stylesheet_directory() . '/path/to/class-tgm-plugin-activation.php';
- *
- * Plugin:
- * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
 require_once 'TGM-Plugin.php';
-
-
-add_action('tgmpa_register', 'my_theme_register_required_plugins');
-
-/**
- * Register the required plugins for this theme.
- *
- * In this example, we register five plugins:
- * - one included with the TGMPA library
- * - two from an external source, one from an arbitrary source, one from a GitHub repository
- * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
- *
- * The variables passed to the `tgmpa()` function should be:
- * - an array of plugin arrays;
- * - optionally a configuration array.
- * If you are not changing anything in the configuration array, you can remove the array and remove the
- * variable from the function call: `tgmpa( $plugins );`.
- * In that case, the TGMPA default settings will be used.
- *
- * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
- */
-function my_theme_register_required_plugins()
-{
+add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+function my_theme_register_required_plugins() {
     /*
      * Array of plugin arrays. Required keys are name and slug.
      * If the source is NOT from the .org repo, then source is also required.
      */
-    $plugins = array(
-        array(
-            'name' => 'Contact Form 7',
-            'slug' => 'contact-form-7',
-            'required' => false,
-        ),
-        array(
-            'name' => 'WP Migrate DB',
-            'slug' => 'wp-migrate-db',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Wp-scss',
-            'slug' => 'wp-scss',
-            'required' => true,
-        ),
-        array(
-            'name' => 'Better WordPress Minify',
-            'slug' => 'bwp-minify',
-            'required' => false,
-        ),
-        array(
-            'name' => 'EWWW Image Optimizer',
-            'slug' => 'ewww-image-optimizer',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Quick Bulk Post & Page Creator',
-            'slug' => 'quick-bulk-post-page-creator',
-            'required' => false,
-        ),
-        array(
-            'name' => 'WP No Category Base',
-            'slug' => 'wp-no-category-base',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Post Thumbnail Editor',
-            'slug' => 'post-thumbnail-editor',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Resize Image After Upload',
-            'slug' => 'resize-image-after-upload',
-            'required' => false,
-        ),
-
-        array(
-            'name' => 'TinyMCE Advanced',
-            'slug' => 'tinymce-advanced',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Duplicate post',
-            'slug' => 'duplicate-post',
-            'required' => false,
-        ),
-        array(
-            'name' => 'AJAX Thumbnail Rebuild',
-            'slug' => 'ajax-thumbnail-rebuild',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Optimize Database after Deleting Revisions',
-            'slug' => 'rvg-optimize-database',
-            'required' => false,
-        ),
-     array(
-            'name' => 'All-in-One WP Migration',
-            'slug' => 'all-in-one-wp-migration',
-            'required' => false,
-        ),
-        array(
-            'name' => 'Advanced Custom Fields: PRO',
-            'slug' => 'advanced-custom-fields-pro',
-            'source' => get_stylesheet_directory() . '/include/plugins/advanced-custom-fields-pro.zip',
-            'required' => false,
-            'version' => '',
-            'force_activation' => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-            'external_url' => 'http://www.advancedcustomfields.com/pro/', // If set, overrides default API URL and points to an external URL.
-        ),
+        $plugins = array(
+            array(
+                'name' => 'Advanced Custom Fields: PRO',
+                'slug' => 'advanced-custom-fields-pro',
+                'source' => get_stylesheet_directory() . '/include/plugins/advanced-custom-fields-pro.zip',
+                'required' => true,
+                'external_url' => 'http://www.advancedcustomfields.com/pro/',
+            ),
+            array(
+                'name' => 'Wp-scss',
+                'slug' => 'wp-scss',
+                'required' => true,
+            ),
+            array(
+                'name' => 'Contact Form 7',
+                'slug' => 'contact-form-7',
+                'required' => false,
+            ),
+            array(
+                'name' => 'WP Migrate DB',
+                'slug' => 'wp-migrate-db',
+                'required' => false,
+            ),
+            array(
+                'name' => 'AJAX Thumbnail Rebuild',
+                'slug' => 'ajax-thumbnail-rebuild',
+                'required' => false,
+            ),
+            array(
+                'name' => 'Better WordPress Minify',
+                'slug' => 'bwp-minify',
+                'required' => false,
+            ),
+            array(
+                'name' => 'EWWW Image Optimizer',
+                'slug' => 'ewww-image-optimizer',
+                'required' => false,
+            ),
+            array(
+                'name' => 'Quick Bulk Post & Page Creator',
+                'slug' => 'quick-bulk-post-page-creator',
+                'required' => false,
+            ),
+            array(
+                'name' => 'Duplicate post',
+                'slug' => 'duplicate-post',
+                'required' => false,
+            ),
+            array(
+                'name' => 'Resize Image After Upload',
+                'slug' => 'resize-image-after-upload',
+                'required' => false,
+            ),
+            array(
+                'name' => 'TinyMCE Advanced',
+                'slug' => 'tinymce-advanced',
+                'required' => false,
+            ),
+            array(
+                'name' => 'Optimize Database after Deleting Revisions',
+                'slug' => 'rvg-optimize-database',
+                'required' => false,
+            ),
 
     );
 
     /*
      * Array of configuration settings. Amend each line as needed.
-     *
-     * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-     * strings available, please help us make TGMPA even better by giving us access to these translations or by
-     * sending in a pull-request with .po file(s) with the translations.
-     *
-     * Only uncomment the strings in the config array if you want to customize the strings.
      */
     $config = array(
-        'id' => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+        'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
         'default_path' => '',                      // Default absolute path to bundled plugins.
-        'menu' => 'tgmpa-install-plugins', // Menu slug.
-        'parent_slug' => 'themes.php',            // Parent menu slug.
-        'capability' => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-        'has_notices' => true,                    // Show admin notices or not.
-        'dismissable' => true,                    // If false, a user cannot dismiss the nag message.
-        'dismiss_msg' => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-        'message' => '',                      // Message to output right before the plugins table.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'parent_slug'  => 'plugins.php',            // Parent menu slug.
+        'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+        'message'      => '',                      // Message to output right before the plugins table.
 
 
     );
 
-    tgmpa($plugins, $config);
+    tgmpa( $plugins, $config );
 }
+
+
+
+
+
+
