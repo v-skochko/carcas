dc89589b4e9a866c9b9d1cae9b125e5046c77df7
Skochko <skochkovs@gmail.com>
2016-06-03 12:19:37 +0300

    Update core.php

--- include/core.php
+++ include/core.php
@@ -14,8 +14,8 @@ function style_js()
         wp_enqueue_script('jquery');
     };
     // wp_enqueue_script('googlemaps', '//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false', array(), '', FALSE);
-    wp_enqueue_script('libs', get_template_directory_uri() . '/js/lib.js');
-    wp_enqueue_script('init', get_template_directory_uri() . '/js/init.js');
+    wp_enqueue_script('libs', get_template_directory_uri() . '/js/lib.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('init', get_template_directory_uri() . '/js/init.js', array('jquery'), '1.0', true);
     wp_enqueue_style('style', get_template_directory_uri() . '/scss/main.css');
 }
 add_action('wp_enqueue_scripts', 'style_js');
