033e2427021efc98f1ad80d9268a3834200e080b
Skochko <skochkovs@gmail.com>
2016-08-18 15:45:00 +0300

    Update header.php

--- header.php
+++ header.php
@@ -5,14 +5,12 @@
     <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui, minimum-scale=1.0, maximum-scale=1.0" />
     <?php /* favicon */ ?>
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo theme(); ?>/img/favicon/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo theme(); ?>/img/favicon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo theme(); ?>/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="<?php echo theme(); ?>/img/favicon/manifest.json">
     <link rel="mask-icon" href="<?php echo theme(); ?>/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ff0000">
+    <meta name="theme-color" content="#eee">
     <?php /* end favicon */ ?>
     <?php wp_head(); ?>
 </head>
