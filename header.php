278dda0ac073d9d376f6008d4a58ceed61442b62
Skochko <skochkovs@gmail.com>
2016-09-01 12:24:48 +0300

    Update header.php

--- header.php
+++ header.php
@@ -4,14 +4,13 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui, minimum-scale=1.0, maximum-scale=1.0" />
-    <?php /* favicon */ ?>
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo theme(); ?>/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="<?php echo theme(); ?>/img/favicon/manifest.json">
-    <link rel="mask-icon" href="<?php echo theme(); ?>/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="theme-color" content="#eee">
-    <?php /* end favicon */ ?>
+	<?php /* favicon */ ?>
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo theme(); ?>/img/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo theme(); ?>/img/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="<?php echo theme(); ?>/img/favicon/manifest.json">
+	<meta name="theme-color" content="#eee">
+	<?php /* end favicon */ ?>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
