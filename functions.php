723367877dbb4fcdb41612d5db59b8a9996ee258
Skochko <skochkovs@gmail.com>
2016-11-05 20:31:20 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -27,3 +27,12 @@ register_nav_menus( array(
 	'second_menu' => 'Second navigation',
 	'foot_menu'   => 'Footer navigation'
 ) );
+function annointed_admin_bar_remove() { 
+ 
+   global $wp_admin_bar; /* Remove their stuff */
+ 
+   $wp_admin_bar->remove_menu('wp-logo');
+ 
+ } 
+ 
+add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
