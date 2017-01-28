ce0d367c9c6d6ebac29fffd643c46aa41d381317
Skochko <Skochkovs@gmail.com>
2017-01-28 23:34:59 +0200

    upd

--- functions.php
+++ functions.php
@@ -30,3 +30,22 @@ register_nav_menus( array(
 	'foot_menu'   => 'Footer navigation'
 ) );
 
+
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+
+	// remove original path (optional)
+	unset($paths[0]);
+
+
+	// append path
+	$paths[] = get_stylesheet_directory() . '/include/acf';
+
+
+	// return
+	return $paths;
+
+}
+
