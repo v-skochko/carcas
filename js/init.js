df3bc1b80767eac205de9b7dbb8d93e574c1282d
Skochko <Skochkovs@gmail.com>
2017-07-20 16:42:42 +0300

    upd

--- js/init.js
+++ js/init.js
@@ -2,24 +2,20 @@ $ = jQuery;
 $(document).ready(function () {
     "use strict";
     $(".mobile_nav").find('.menu-item-has-children>a').after('<i class="i-down sub-anchor"></i>');
-
     $('.burger').click(function () {
         $(this).toggleClass('is-active');
         $("body").toggleClass('resp_menu_active');
         return false;
     });
-    
-    
-       $('.burger, #menuOpen').clickOff(function () {
-        $("#menuOpen").removeClass("opened");
+    $('.resp_container').clickOff(function () {
+        $('.burger').removeClass('is-active');
 
+        $("body").removeClass('resp_menu_active');
+        return false;
     });
-    
-    
     $(".menu-item-has-children i").click(function (event) {
         $(this).toggleClass('i-up').next().slideToggle("fast");
     });
-
     /*Contact form7 - close validation error on click*/
     $(this).on('click', '.wpcf7-not-valid-tip', function () {
         $(this).prev().trigger('focus');
@@ -27,49 +23,41 @@ $(document).ready(function () {
             $(this).remove();
         });
     });
-
-
     $(".wpcf7-response-output").click(function (event) {
         $(this).slideUp(400);
     });
-
-
     // $('select').selectric({
     //     disableOnMobile: false
     // });
-
-
 });
 /* end ready*/
 $(window).on('load', function () {
-    // var main_slider = new Swiper('.main_slider', {
-    //     loop: true,
-    //     paginationClickable: true,
-    //     pagination: '.main_slider .swiper-pagination',
-    //     prevButton: '.main_slider .swiper-button-prev',
-    //     nextButton: '.main_slider .swiper-button-next',
-    //     fade: {crossFade: true},
-    //     virtualTranslate: true,
-    //     autoplay: 5000,
-    //     speed: 1000,
-    //     effect: 'fade',
-    //     spaceBetween: 40,
-    //     onInit: function (swiper) {
-    //         $("#top_home_slider").addClass('loaded')
-    //     },
-     paginationBulletRender: function (index, className) {
-            var slide = $('.benefits-slider .swiper-container').find('.swiper-slide')[index];
-            return '<span class="' + className + '">' + $(slide).attr('data-desc') + '</span>';
+    var main_slider = new Swiper('.main_slider', {
+        loop: true,
+        paginationClickable: true,
+        pagination: '.main_slider .swiper-pagination',
+        prevButton: '.main_slider .swiper-button-prev',
+        nextButton: '.main_slider .swiper-button-next',
+        fade: {crossFade: true},
+        virtualTranslate: true,
+        autoplay: 5000,
+        speed: 1000,
+        effect: 'fade',
+        spaceBetween: 40,
+        onInit: function (swiper) {
+            $("body").addClass('main_slider_loaded')
+        },
+        paginationBulletRender: function (index, className) {
+            var slide = $('.swiper-container').find('.swiper-slide')[index];
+            return '<span class="' + className + '">' + $(slide).attr('data-description') + '</span>';
+        },
+        breakpoints: {
+            960: {
+                slidesPerView: 2
+            },
+            480: {
+                slidesPerView: 1
+            }
         }
-    //     breakpoints: {
-    //         960: {
-    //             slidesPerView: 2
-    //         },
-    //         480: {
-    //             slidesPerView: 1
-    //         }
-    //     }
-    // });
-    
-    
+    });
 });
--- js/lib.js
+++ js/lib.js
@@ -1,16 +1,7 @@
-$.fn.clickOff = function (callback, selfDestroy) {
-    var clicked = false;
-    var parent = this;
-    var destroy = selfDestroy || true;
-    parent.click(function () {
-        clicked = true;
-    });
-    $(document).click(function (event) {
-        if (!clicked) {
-            callback(parent, event);
-        }
-        if (destroy) {
-        }
-        clicked = false;
-    });
-};
+//swiper source   https://raw.githubusercontent.com/nolimits4web/Swiper/master/dist/js/swiper.jquery.min.js
+//fancybox source   https://raw.githubusercontent.com/fancyapps/fancybox/master/dist/jquery.fancybox.min.js
+
+
+
+//clickOff
+$.fn.clickOff = function (callback, selfDestroy) { var clicked = false; var parent = this; var destroy = selfDestroy || true; parent.click(function () { clicked = true; }); $(document).click(function (event) { if (!clicked) { callback(parent, event); } if (destroy) { } clicked = false; }); };
