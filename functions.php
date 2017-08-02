56be92eb3b093bd008379ab0cbd5d45d6654d475
Skochko <skochkoVs@gmail.com>
2017-08-02 11:35:44 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -29,3 +29,30 @@ register_nav_menus( array(
 	'second_menu' => 'Second navigation',
 	'foot_menu'   => 'Footer navigation'
 ) );
+
+function fb_move_admin_bar() {
+    echo '
+    <style type="text/css">
+    body {
+    margin-top: -28px;
+    padding-bottom: 28px;
+    }
+    body.admin-bar #wphead {
+       padding-top: 0;
+    }
+    body.admin-bar #footer {
+       padding-bottom: 28px;
+    }
+    #wpadminbar {
+        top: auto !important;
+        bottom: 0;
+    }
+    #wpadminbar .quicklinks .menupop ul {
+        bottom: 28px;
+    }
+    </style>';
+}
+// on backend area
+add_action( 'admin_head', 'fb_move_admin_bar' );
+// on frontend area
+add_action( 'wp_head', 'fb_move_admin_bar' );
