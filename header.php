d146779fd4a3d3820f6236e40189594e11c2e67d
Skochko <skochkoVs@gmail.com>
2017-08-02 13:50:37 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> <?php if ( is_user_logged_in() ) {
+	echo 'style="margin-top: 0!important; margin-bottom: 32px!important;"';
+} ?>
+>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <title><?php wpa_title(); ?></title>
