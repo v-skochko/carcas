28031a6aa06d391028f2f8660453e23214af0287
Skochko <skochkoVs@gmail.com>
2017-08-10 16:41:32 +0300

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -1,6 +1,7 @@
 //swiper source   https://raw.githubusercontent.com/nolimits4web/Swiper/master/dist/js/swiper.jquery.min.js
 //fancybox source   https://raw.githubusercontent.com/fancyapps/fancybox/master/dist/jquery.fancybox.min.js
 //selectric https://raw.githubusercontent.com/lcdsantos/jQuery-Selectric/master/public/jquery.selectric.min.js
+//datepicker https://github.com/amsul/pickadate.js/edit/master/lib/compressed/picker.js  https://raw.githubusercontent.com/amsul/pickadate.js/master/lib/compressed/picker.date.js
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //clickOff
 $.fn.clickOff = function (callback, selfDestroy) { var clicked = false; var parent = this; var destroy = selfDestroy || true; parent.click(function () { clicked = true; }); $(document).click(function (event) { if (!clicked) { callback(parent, event); } if (destroy) { } clicked = false; }); };
