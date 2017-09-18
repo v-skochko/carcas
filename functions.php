e38bfe520bf20a23c67a237c5e4d89dc8066fda0
Skochko <skochkoVs@gmail.com>
2017-09-18 15:43:47 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -44,3 +44,17 @@ if(defined('WPCF7_VERSION')) {
 	}
 	add_filter( 'wpcf7_form_elements', 'maybe_reset_autop' );
 }
+
+
+add_action( 'wp_enqueue_scripts', 'mgt_dequeue_stylesandscripts', 100 );
+
+function mgt_dequeue_stylesandscripts() {
+	if ( class_exists( 'woocommerce' ) ) {
+		wp_dequeue_style( 'select2' );
+		wp_deregister_style( 'select2' );
+
+		wp_dequeue_script( 'select2' );
+		wp_deregister_script( 'select2' );
+
+	}
+} 
