61515f1ec50500d27da8c0b34c5258dbc329f5c7
Skochko <skochkoVs@gmail.com>
2017-11-09 11:07:27 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -33,4 +33,22 @@ $(window).on('load', function () {
     //         }
     //     }
     // });
+    
+    //new
+        var testimonials_slider = new Swiper('.testimonials_slider', {
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 4000
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev'
+        }
+    });
 });
