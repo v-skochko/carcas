11484b51b6831cc7ef178bc67911128f1b40cd26
Skochko <skochkoVs@gmail.com>
2017-12-10 19:45:54 +0200

    Update global-flexible.php

--- components/global-flexible.php
+++ components/global-flexible.php
@@ -11,6 +11,19 @@
                 <div class="<?php the_sub_field( 'width' ); ?> sm-12 simple_wysiwyg  ">
 					<?php the_sub_field( 'simple_wysiwyg_content' ); ?>
                 </div>
+	
+			<?php /* accordion */
+            elseif ( get_row_layout() == 'accordion' ): ?>
+				<?php if ( have_rows( 'items' ) ): ?>
+                    <div class="items">
+						<?php
+						while ( have_rows( 'items' ) ) : the_row();
+							the_sub_field( 'title' );
+							the_sub_field( 'a_content' );
+						endwhile; ?>
+                    </div>
+				<?php endif; ?>
+	
 			<?php /* other_group */
             elseif ( get_row_layout() == 'other_group' ): ?>
 				<?php the_sub_field( 'other_group_content' ); ?>
