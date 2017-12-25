6b93950bd3bf43fea172975c7ef931f3691f518b
Skochko <skochkoVs@gmail.com>
2017-12-25 18:05:54 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,31 +9,7 @@ $(document).ready(function () {
 });
 /* end ready*/
 $(window).on('load', function () {
-    // var main_slider = new Swiper('.main_slider', {
-    //     loop: true,
-    //     paginationClickable: true,
-    //     pagination: '.main_slider .swiper-pagination',
-    //     prevButton: '.main_slider .swiper-button-prev',
-    //     nextButton: '.main_slider .swiper-button-next',
-    //     effect: 'fade',
-    //     fade: {crossFade: true},
-    //     virtualTranslate: true,
-    //     autoplay: 5000,
-    //     speed: 1000,
-    //     spaceBetween: 40,
-    //     onInit: function (swiper) {
-    //         $("body").addClass('main_slider_loaded')
-    //     },
-    //    paginationBulletRender: function (swiper, index, className, swiperprocessstep) {
-    //       return '<span class="' + className + ' "> ' + swiper.slides.eq(index).attr('data-name') + '</span>';
-    //    },
-    //     breakpoints: {
-    //         480: {
-    //             slidesPerView: 1
-    //         }
-    //     }
-    // });
-    
+   
     //new
         var testimonials_slider = new Swiper('.testimonials_slider', {
         loop: true,
@@ -49,6 +25,11 @@ $(window).on('load', function () {
         navigation: {
             nextEl: '.swiper-next',
             prevEl: '.swiper-prev'
+        },
+              on: {
+            init: function () {
+                $('.work_bottom_slider').addClass('slider_loaded');
+            }
         }
     });
 });
