770c12729891964e16dca039a8f4ba0dce95b633
Skochko <skochkoVs@gmail.com>
2018-01-31 15:11:31 +0200

    Update core.php

--- include/core.php
+++ include/core.php
@@ -369,14 +369,15 @@ function image_src( $id, $size = 'full', $background_image = false, $height = fa
 		return $background_image ? 'background-image: url(\'' . $image[0] . '\');' . ( $height ? 'height:' . $image[2] . 'px' : '' ) : $image[0];
 	}
 }
-/* [button link="#"  align="alignleft"]Align left button[/button]
+/* [button link="#"  class="alignleft"]Align left button[/button]
    ========================================================================== */
 function sButton( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'link'  => '#',
-		'align' => 'none',
+		'class' => 'none',
 	), $atts ) );
-	return '<a class="btn ' . $align . '" href="' . $link . '">' . do_shortcode( $content ) . '</a>';
+
+	return '<a class="btn ' . $class . '" href="' . $link . '">' . do_shortcode( $content ) . '</a>';
 }
 add_shortcode( 'button', 'sButton' );
 /* Remove WordPress logo & pages from Admin bar
