dbc0c3dbe9f6b8d5a0207784da0ed7705f505b94
Skochko <skochkoVs@gmail.com>
2018-02-26 17:52:51 +0200

    Update woocommerce.php

--- include/woocommerce.php
+++ include/woocommerce.php
@@ -8,4 +8,14 @@ function mgt_dequeue_stylesandscripts() {
 		wp_deregister_script( 'select2' );
 	}
 }
+
+add_action( 'after_setup_theme', 'yourtheme_setup' );
+ 
+function yourtheme_setup() {
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+}
+
+
 ?>
