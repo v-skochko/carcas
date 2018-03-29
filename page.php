c16007270b7bb6316909db4610466d7c143807fa
Skochko <skochkoVs@gmail.com>
2018-03-29 16:34:50 +0300

    Update page.php

--- page.php
+++ page.php
@@ -1,6 +1,5 @@
-<?php get_header();
-global $post; ?>
-<div id="content" class="row page_id_<?php echo $post->ID ?>">
+<?php get_header(); ?>
+<div id="content" class="row page_id_<?php the_ID() ?>">
     <main>
         <h1 class="page-title"><?php the_title(); ?></h1>
         <div class="wysiwyg">
