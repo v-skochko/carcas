47aeb310d0e148fc8c17e658c8c982459d6c963d
Skochko <skochkoVs@gmail.com>
2018-04-16 14:23:12 +0300

    Update woocommerce.php

--- include/woocommerce.php
+++ include/woocommerce.php
@@ -11,6 +11,14 @@ function mgt_dequeue_stylesandscripts() {
 
 add_action( 'after_setup_theme', 'yourtheme_setup' );
 
+
+//declare WC support
+function aventurine_child_wc_support() {
+	add_theme_support( 'woocommerce' );
+}
+
+add_action( 'after_setup_theme', 'aventurine_child_wc_support' );
+
 function yourtheme_setup() {
 	add_theme_support( 'wc-product-gallery-zoom' );
 	add_theme_support( 'wc-product-gallery-lightbox' );
@@ -18,4 +26,12 @@ function yourtheme_setup() {
 }
 
 
-?>
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+
+function new_loop_shop_per_page( $cols ) {
+	// $cols contains the current number of products per page based on the value stored on Options -> Reading
+	// Return the number of products you wanna show per page.
+	$cols = 12;
+
+	return $cols;
+}
