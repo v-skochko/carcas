933a376a599b6a5140ec18e888b3debbafd11a47
Skochko <skochkoVs@gmail.com>
2019-03-19 16:05:09 +0200

    Update page.php

--- page.php
+++ page.php
@@ -2,12 +2,17 @@
 <div id="content" class="row">
     <main>
         <h1 class="page-title"><?php the_title(); ?></h1>
+         <?php if ('' !== get_post()->post_content) { ?>
+                
+            
+        
         <div class="wysiwyg">
             <?php if (have_posts()): while (have_posts()): the_post();
                 the_content();
             endwhile;endif;
             ?>
         </div>
+        <?php } ?>
     </main>
 </div>
 <?php get_footer(); ?>
